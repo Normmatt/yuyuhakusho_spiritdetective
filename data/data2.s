f7e1a26361ddac5806d586cbc3a0ad4efb52c2b3
Normmatt <normmatt234@gmail.com>
2021-01-14 16:15:10 +1300

    Fix mov32 macro. Split library rodata from data appended at end of ROM.

--- data/data2.s
+++ data/data2.s
@@ -1,25 +1,4 @@
     .section .data
-    
-
-    .global gUnknown_08076130
-gUnknown_08076130:
-    .incbin "baserom.gba", 0x00076124, 0xC
-
-    .global eeprom_list
-eeprom_list:
-    .incbin "baserom.gba", 0x00076130, 0xC
-
-    .global gUnknown_0807613C
-gUnknown_0807613C:
-    .incbin "baserom.gba", 0x0007613C, 0xC
-
-    .global epMaxTime
-epMaxTime:
-    .incbin "baserom.gba", 0x00076148, 0x68
-    
-    .global gUnknown_080761B0
-gUnknown_080761B0:
-    .space 0xB890
 
     .global gUnknown_08081A40
 gUnknown_08081A40:
